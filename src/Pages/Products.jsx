77812c05c03385f72bbafd0f78b4febf4e26d04a
Zahra Nejati <nejatizahra4@gmail.com>
2022-09-28 12:24:22 +0330

    redux-devtools / connect products state

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,15 +1,20 @@
 import Product from "../Components/Product";
-import * as pro from "../db";
-const Products = () => {
-  console.log(pro.products);
+
+import { connect } from "react-redux";
+const Products = ({ products }) => {
   return (
     <div>
       <h2>this is products page</h2>
       <section className="flex flex-col items-center justify-center">
-        <Product producs={pro.products} />
+        <Product producs={products} />
       </section>
     </div>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+    products: state.shop.products,
+  };
+};
 
-export default Products;
+export default connect(mapStateToProps)(Products);
