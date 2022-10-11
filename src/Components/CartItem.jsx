7e15943e17bde3f9fb2fe1b45ce673aa75f9cc5c
Zahra Nejati <nejatizahra4@gmail.com>
2022-10-11 14:15:53 +0330

    qty input adJustQty

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { connect } from "react-redux";
-import { removeFromCart } from "../Redux/Shopping/Shopping-action";
-const CartItem = ({ product, removeFromCart }) => {
+import { removeFromCart, adjustQty } from "../Redux/Shopping/Shopping-action";
+const CartItem = ({ product, removeFromCart, adjustQty }) => {
+  const [input, setInput] = useState(product.qty);
+  const changeHandler = (e) => {
+    setInput(e.target.value);
+    adjustQty(product.id, input);
+  };
   return (
     <div className="flex items-center justify-between bg-red-200 gap-x-8 min-w-full">
       <div className="h-40 w-40 bg-slate-500"></div>
@@ -11,7 +17,8 @@ const CartItem = ({ product, removeFromCart }) => {
       </div>
       <div className="flex flex-col gap-y-12 mr-2">
         <input
-          value={product.qty}
+          onChange={changeHandler}
+          value={input}
           min={1}
           type="number"
           className="w-12 h-8"
@@ -25,6 +32,7 @@ const CartItem = ({ product, removeFromCart }) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     removeFromCart: (id) => dispatch(removeFromCart(id)),
+    adjustQty: (id, value) => dispatch(adjustQty(id, value)),
   };
 };
 
